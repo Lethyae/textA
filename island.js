bf9f17dda6c2390a048ad5e7b925895346a34819
Lethyae <leslie230894@gmail.com>
2022-04-05 19:44:18 +0200

    More tests

--- island.js
+++ island.js
@@ -1,10 +1,16 @@
-let x = 0;
-let y = 0;
+let middleX = width / 2;
+let middleY = height / 2;
+
+function mainMenu() {
+  background(0, 0, 0);
+
+  fill(255, 255, 255);
+  textAlign(CENTER, CENTER);
+  textSize(50);
+  text("Welcome", middleX, height / 10);
+}
 
 function draw() {
   clear();
-  rect(x, y, 100, 100);
-
-  x++;
-  y++;
+  mainMenu();
 }
