d24f7ba713174d8784b23db75aa59bb2544d19fc
Lethyae <leslie230894@gmail.com>
2022-04-14 19:39:13 +0200

    Some sketches and tests

---
+++ test.js
@@ -0,0 +1,13 @@
+// user input test
+/*
+function setup() {
+  let inp = createInput("");
+  inp.position(0, 0);
+  inp.size(100);
+  inp.input(myInputEvent);
+}
+
+function myInputEvent() {
+  console.log("you are typing: ", this.value());
+}
+*/
