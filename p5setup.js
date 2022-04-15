aeaeee6aa9e179b53eb3c7e1572c78e1ddaf9a0a
Leslie J. Hartmann <leslie230894@gmail.com>
2022-04-15 16:10:45 +0200

    Start page with link added
    Hover button, text, decorative banners

--- p5setup.js
+++ p5setup.js
@@ -1,21 +1,21 @@
 function setup() {
   frameRate(30);
 
-  var cnv = createCanvas(windowWidth, windowHeight);
-  // position of canvas in window
-  var x = 0;
-  var y = 0;
+  var cnv = createCanvas(windowWidth / 1.3, windowHeight / 1.25);
+  // position of canvas in window at center of window
+  var x = (windowWidth - windowWidth / 1.3) / 2;
+  var y = (windowHeight - windowHeight / 1.25) / 2;
   cnv.position(x, y);
-  background(0, 0, 0);
+  background(255, 255, 255);
 }
 
 window.addEventListener("resize", function () {
-  resizeCanvas(windowWidth, windowHeight);
-  x = 0;
-  y = 0;
+  resizeCanvas(windowWidth / 1.3, windowHeight / 1.25);
+  x = (windowWidth - windowWidth / 1.3) / 2;
+  y = (windowHeight - windowHeight / 1.25) / 2;
   clear();
 });
 
 new p5();
-var width = windowWidth;
-var height = windowHeight;
+var width = windowWidth / 1.3;
+var height = windowHeight / 1.2;
